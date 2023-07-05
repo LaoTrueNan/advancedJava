420e8e7a4cc50dd199dc39d9ca8f256982fea42f
siyhong <leonard.qiang@gmail.com>
2023-07-05 16:37:40 +0800

    通过3个用例子

--- src/gzq/byd/com/dynamic/no1/CompressString.java
+++ src/gzq/byd/com/dynamic/no1/CompressString.java
@@ -11,13 +11,40 @@ public class CompressString {
     /* 1<=s.length<=100 */
     /* 0<=k<=s.length */
     public int getLengthOfOptimalCompression(String s, int k){
+        // 算出删除n个的长度,然后再根据其计算n+1
         // 单独记录某个字符被剔除掉之后的压缩结果,但是怎么考虑k呢?
-        // 一个字符时输出1
+        // 一个字符时输出1,也就是长度1省略  a->a 而不是a1
+        // 0-k遍历,留一个下标表示删除n位
+        // 用某个状态来模拟
+        int ret  = s.length();
+        int left = 0,cnt = 1;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < s.length()-1; i++) {
+            if(chars[i]==chars[i+1]){
+                cnt++;
+            }else{
+                left = i+1;
+                if(cnt>1){
+                    ret = ret + String.valueOf(cnt).length() - cnt + 1;
+                }
+                cnt = 1;
+            }
+        }
+        if(left == 0){
+            ret = ret + String.valueOf(cnt).length() - cnt + 1;
+        }
+        // bts[n]表示删除n位之后的最短长度
+        int[] bts = new int[k+1];
 
+        bts[0] = ret;
+//        for (int i = 1; i < k + 1; i++) {
+//            bts[i] = bts[i-1];
+//        }
         // 两个相同字符时输出
 
         // 两个不同字符时输出
-        return s.length();
+
+        return ret;
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
