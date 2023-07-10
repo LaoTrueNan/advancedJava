38cd1e1c36a12f6491657c2c024dcaa67590f63a
siyhong <leonard.qiang@gmail.com>
2023-07-10 16:05:56 +0800

    开始图论

---
+++ src/gzq/byd/com/graph/HelloGraph.java
@@ -0,0 +1,30 @@
+/*
+ * author guo
+ * year 2023
+ */
+package gzq.byd.com.graph;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * 洛谷P1359 租用游艇
+ */
+public class HelloGraph {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        Map<Integer, List<Integer>> map = new HashMap<>(n);
+
+        for (int i = 1; i < (n-1)*n/2; i++) {
+            for (int j = i+1;j<=n;j++){
+
+                map.put(i,)
+            }
+        }
+    }
+}
